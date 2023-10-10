3afc694b6300d1e3d8dc9c699df32858bed3b957
KurniaShandi <80150569+KurniaShandi@users.noreply.github.com>
2023-10-10 23:09:29 +0700

    Initial commit

---
+++ app/Http/Resources/PostDetailResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    // public function toArray(Request $request): array
+    // {
+    //     return [
+    //         //key -> $this->namaColumn
+    //         'id' => $this->id,
+    //         'title' => $this->title,
+    //         'author' => $this->author,
+    //     ];
+    // }
+
+    public function toArray(Request $request): array
+    {
+        return [
+            //key -> $this->namaColumn
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'writer' => $this->writer->username,
+        ];
+    }
+
+}
